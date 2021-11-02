c1ddce0ee6764771c7c39a87dcf93bc200ba977f
Luke Eyles <modularluke@gmail.com>
2021-11-02 17:06:46 +1100

    change marker to line

--- src/wamv_tf_publisher.cpp
+++ src/wamv_tf_publisher.cpp
@@ -9,6 +9,7 @@
 
 std::mutex mtx;
 gazebo_msgs::ModelStates _modelStates;
+visualization_msgs::Marker _marker;
 
 void modelStatesCallback(const gazebo_msgs::ModelStates &msg)
 {
@@ -27,6 +28,18 @@ int main(int argc, char **argv)
     static tf2_ros::TransformBroadcaster br;
     geometry_msgs::TransformStamped wamvTransform;
     geometry_msgs::TransformStamped wamv2Transform;
+
+    // set marker data
+    _marker.header.frame_id = "world";
+    _marker.header.stamp = ros::Time();
+    _marker.id = 0;
+    _marker.type = visualization_msgs::Marker::LINE_STRIP;
+    _marker.action = visualization_msgs::Marker::ADD;
+    _marker.scale.x = 0.1;
+    _marker.color.a = 1.0;
+    _marker.color.r = 1.0;
+    _marker.color.g = 0.0;
+    _marker.color.b = 0.0;
 
     ros::Rate loop_rate(60);
     while (ros::ok && _modelStates.name.size() < 1)
@@ -80,26 +93,11 @@ int main(int argc, char **argv)
         br.sendTransform(wamvTransform);
         br.sendTransform(wamv2Transform);
 
-        // publish wamv markers
-        visualization_msgs::Marker marker;
-        marker.header.frame_id = "world";
-        marker.header.stamp = ros::Time();
-        marker.id = count+100;
-        marker.type = visualization_msgs::Marker::SPHERE;
-        marker.action = visualization_msgs::Marker::ADD;
-        marker.pose.position = modelStates.pose.at(wamv).position;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
-        marker.scale.x = 0.1;
-        marker.scale.y = 0.1;
-        marker.scale.z = 0.1;
-        marker.color.a = 1.0;
-        marker.color.r = 1.0;
-        marker.color.g = 0.0;
-        marker.color.b = 0.0;
-        marker_pub.publish(marker);
+        // push new point to line marker
+        _marker.points.push_back(modelStates.pose.at(wamv).position);
+
+        // publish wamv marker
+        marker_pub.publish(_marker);
 
         count++;
         ros::spinOnce();
